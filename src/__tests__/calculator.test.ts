dc56d63cfab6ae6471d3784b764780381335ba92
vm9054 <vm9054@att.com>
2024-08-04 11:24:51 +0530

    Added Additonal test cases and it's code implementation

--- src/__tests__/calculator.test.ts
+++ src/__tests__/calculator.test.ts
@@ -7,7 +7,21 @@ describe('Calculator', () => {
     calculator = new Calculator();
   });
 
-  test('should add two numbers', () => {
+  it('should add two numbers', () => {
     expect(calculator.add(2, 3)).toBe(5);
   });
+
+  it('should subtract two numbers', () => {
+    expect(calculator.subtract(5, 3)).toBe(2);
+  });
+
+  test('should multiply two numbers', () => {
+    expect(calculator.multiply(2, 3)).toBe(6);
+  });
+
+  test('should divide two numbers', () => {
+    expect(calculator.divide(6, 3)).toBe(2);
+  });
+
+
 });
--- src/calculator.ts
+++ src/calculator.ts
@@ -2,4 +2,16 @@ export class Calculator {
     add(a: number, b: number): number {
         return a + b;
     }
+
+    subtract(a: number, b: number): number {
+        return a - b;
+    }
+
+    multiply(a: number, b: number): number {
+        return a * b;
+    }
+
+    divide(a: number, b: number): number {
+        return a / b;
+    }
 }
