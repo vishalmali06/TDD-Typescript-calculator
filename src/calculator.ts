253d5d11117477359281b1a3bfbf7b363f0dd2b9
Vishal Mali <26736191+vishalmali06@users.noreply.github.com>
2024-08-20 15:40:11 +0530

    Update calculator.ts add method

--- src/calculator.ts
+++ src/calculator.ts
@@ -4,9 +4,16 @@ export class Calculator {
         if (input === '') return 0;
 
         let delimiter = /,|\n/;
+        let isMultiplication = false;
+
         if (input.startsWith('//')) {
             const delimiterEnd = input.indexOf('\n');
             delimiter = new RegExp(input.substring(2, delimiterEnd));
+
+            if (input.substring(2, delimiterEnd) === '@') {
+                isMultiplication = true;
+            }
+
             input = input.substring(delimiterEnd + 1);
         }
 
@@ -15,6 +22,10 @@ export class Calculator {
 
         if (negatives.length > 0) {
             throw new Error(`negative numbers not allowed: ${negatives.join(', ')}`);
+        }
+
+        if (isMultiplication) {
+            return numbers.reduce((product, num) => product * num, 1);
         }
 
         return numbers.reduce((sum, num) => sum + num, 0);
